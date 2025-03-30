4c192e4923b8ee456ad894f7b374df52268562b1
Piero Dev <pierodev21@gmail.com>
2025-03-30 11:07:30 -0500

    feat: install nvim-emmet for wrap tags

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "olrtg/nvim-emmet",
+        config = function()
+            vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation,
+                { desc = "Wrap tags" })
+        end,
+    },
+}
