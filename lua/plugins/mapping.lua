2ced4e1d73d0addc1964dac0e8cb093a737ffb69
Piero Dev <pierodev21@gmail.com>
2025-03-29 20:20:20 -0500

    feat(mapping): use <S-h> and <S-l> to move between buffers

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -5,6 +5,16 @@ return {
         ---@type AstroCoreOpts
         opts = {
           mappings = {
+            n = {
+                ["<S-h>"] = {
+                    function() require("astrocore.buffer").nav(vim.v.count1) end,
+                    desc = "Next buffer",
+                },
+                ["<S-l>"] = {
+                    function() require("astrocore.buffer").nav(-vim.v.count1) end,
+                    desc = "Previous buffer",
+                },
+            },
             i = {
                 ["<C-s>"] = { "<Esc>:w!<cr>", desc = "Save File" },
             }
