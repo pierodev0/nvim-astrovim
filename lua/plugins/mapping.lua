0713cd9eec72bc4806cff4db732abd4a6f9204cb
Piero Dev <pierodev21@gmail.com>
2025-03-29 20:39:35 -0500

    chore(format): format mapping file

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -1,31 +1,32 @@
 -- Source: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/_astrocore_mappings.lua
 return {
-    {
-        "AstroNvim/astrocore",
-        ---@type AstroCoreOpts
-        opts = {
-          mappings = {
-            n = {
-                ["<S-h>"] = {
-                    function() require("astrocore.buffer").nav(vim.v.count1) end,
-                    desc = "Next buffer",
-                },
-                ["<S-l>"] = {
-                    function() require("astrocore.buffer").nav(-vim.v.count1) end,
-                    desc = "Previous buffer",
-                },
-                ["<Leader><space>"] = {
-                    function()
-                      require("snacks").picker.files {
-                        hidden = vim.tbl_get((vim.uv or vim.loop).fs_stat ".git" or {}, "type") == "directory",
-                      }
-                    end,
-                    desc = "Find files",
-                  },            },
-            i = {
-                ["<C-s>"] = { "<Esc>:w!<cr>", desc = "Save File" },
-            }
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        n = {
+          ["<S-h>"] = {
+            function() require("astrocore.buffer").nav(vim.v.count1) end,
+            desc = "Next buffer",
+          },
+          ["<S-l>"] = {
+            function() require("astrocore.buffer").nav(-vim.v.count1) end,
+            desc = "Previous buffer",
+          },
+          ["<Leader><space>"] = {
+            function()
+              require("snacks").picker.files {
+                hidden = vim.tbl_get((vim.uv or vim.loop).fs_stat ".git" or {}, "type") == "directory",
+              }
+            end,
+            desc = "Find files",
           },
         },
+        i = {
+          ["<C-s>"] = { "<Esc>:w!<cr>", desc = "Save File" },
+        }
       },
+    },
+  },
 }
